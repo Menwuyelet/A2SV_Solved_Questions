5dca9a94340b557e9b2821e02ce7628849444af9
Menu Temesgen <menutemesgen@gmail.com>
2026-02-04 15:07:46 +0300

    (Solved): solved the Division problem on hacker rank.

--- AritmeticOpration.py
+++ AritmeticOpration.py
@@ -1,8 +1,8 @@
 """Read two integers from stdin and print their sum, difference, and product."""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     a = int(input())
     b = int(input())
-    print(a+b)
-    print(a-b)
-    print(a*b)
+    print(a + b)
+    print(a - b)
+    print(a * b)
---
+++ Division.py
@@ -0,0 +1,8 @@
+"""Takes two intiger inputs and prints their intiger division and float division."""
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+    print(a // b)
+    print(a / b)
