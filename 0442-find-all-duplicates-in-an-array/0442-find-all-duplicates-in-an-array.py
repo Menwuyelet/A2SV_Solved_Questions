0d63afdacf0d0b4d2921110ba94447afac1f9799
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-05 19:12:04 +0300

    Time: 37 ms (43.51%), Space: 35.7 MB (8.64%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -2,10 +2,10 @@
 - The question: given a list of numbers we are tasked to find numbers that appear twice on the list.
                - the solution should run on O(n) time and O(1) space additional to the space used to store the output.
 - Solution:
-    - we can solve this problem by iterating through the list one time and store the 
+    - we can solve this problem by iterating through the list one time and store their count.
 -  Time and Space complexity:
-    - Time = using sort: O(n log n + (n+1)) = O(n log n),  using set: O(n + n+1) = O(n)
-    - space = using sort: O(n), using set: O(n)
+    - Time =  O(n)
+    - space = O(n)
 """
 
 class Solution:
