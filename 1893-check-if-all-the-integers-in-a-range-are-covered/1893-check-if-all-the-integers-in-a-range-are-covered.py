e732c90b499186cab20c28b3866fc9e0d28d58a3
Menu Temesgen <menutemesgen@gmail.com>
2026-02-05 13:10:13 +0300

    (Format): updated the code format on some problems

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,5 +1,5 @@
 """
-- The question: given list of ranges and two integers left and right we are being asked to determine if 
+- The question: given list of ranges and two integers left and right we are being asked to determine if
 - the numbers between the left and right inclusive are betwen at least one of the ranges in side of ranges list.
 - Solution:
     - the brute force solution would be creating a continuos list from the ranges in the ranges list
@@ -7,20 +7,22 @@
     - this would not be feasible for larger inputs but since our constraints show that the input size is atmost 50 we can do that.
     - to make it slightly ifficient we use set instead of list to store the generated list to check membership.
 -  Time and Space complexity:
-    - Time => building the set -> O(n*50), since m <= 50, which is O(k), k = right - left + 1 <= 50  
+    - Time => building the set -> O(n*50), since m <= 50, which is O(k), k = right - left + 1 <= 50
             - so the total would be O(n+k) we can remove the k because the k is constant and less than 50,
             - so it will be O(n)
     - space = O(n), due to the creation of the set
 """
+
+
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
-    
+
         ranges_set = set()
         for start, end in ranges:
-            for i in range(start, end+1):
+            for i in range(start, end + 1):
                 ranges_set.add(i)
-        
-        for num in range(left, right+1):
+
+        for num in range(left, right + 1):
             if num not in ranges_set:
                 return False
         return True
--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -3,15 +3,16 @@
 - Solution:
     - so as we can see there are two possible ways this could go, the first n being even and the second n being odd.
     - if n is even the input n it self is our answer since it is a multiple of it self (n*1) and it is also a multiple of 2 since it is even.
-    - else n is odd, the smallest integer that is both multiple of n and 2 is n*2. 
+    - else n is odd, the smallest integer that is both multiple of n and 2 is n*2.
     - so we check the parity of the n and we return n or n*2.
 -  Time and Space complexity:
     - Time = O(1)
     - space = O(1)
 """
 
+
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         if n % 2 == 0:
             return n
-        return n*2
+        return n * 2
