f645f610bf19ee5dc3dc3203c6bc341628d74ee7
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-05 15:52:33 +0300

    Time: 8 ms (61.91%), Space: 20 MB (15.53%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,42 +1,29 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        # sorted_list1 = sorted(list1)
-        # sorted_list2 = sorted(list2)
-        # list1Ptr, list2Ptr = 0, 0
-        # counter = dict(int)
-        # minIndex = 1000
-        # while list1Ptr < len(list1) and list2Ptr < len(list2):
-        #     if sorted_list1[list1Ptr] == sorted_list2[list2Ptr]:
-        #         counter[sorted_list1[list1Ptr]] = listPtr + list2Ptr
-        #         if list1Ptr + list2Ptr < minIndex:
-        #             minIndex = list1Ptr + list2Ptr
-        #         list1Ptr += 1
-        #         list2Ptr += 1
-        #     else:
-        #         if sorted_list1[list1Ptr] > sorted_list2[list2Ptr]:
-        #             list2Ptr += 1
-        #         else:
-        #             list1Ptr += 1
         index_dic1 = {}
         index_dic2 = {}
-        list1_set = set()
-        list2_set = set(list2)
+        # list1_set = set()
+        # list2_set = set(list2)
         ans = []
+
         for index, word in enumerate(list1):
             index_dic1[word] = index
-            list1_set.add(word)
+            # list1_set.add(word)
+
         for index, word in enumerate(list2):
             index_dic2[word] = index
-            list2_set.add(word)
+            # list2_set.add(word)
 
-        intersection_set = list1_set & list2_set
+        # intersection_set = list1_set & list2_set
         
-        counter = defaultdict(str)
+        # counter = defaultdict(str)
         minIndex = 10000
-        for word in intersection_set:
-            minIndex = min(minIndex, index_dic1[word] + index_dic2[word])
-
-        for word in intersection_set:
+        common = set()
+        for word in index_dic1.keys():
+            if word in index_dic2.keys():
+                minIndex = min(minIndex, index_dic1[word] + index_dic2[word])
+                common.add(word)
+        for word in common:
             if index_dic1[word] + index_dic2[word] == minIndex: ans.append(word)
         return ans
             
