326054a24099f7852dbd56faeadbd8a4a7c08f30
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-05 14:28:18 +0300

    Time: 3 ms (62.39%), Space: 21.2 MB (5.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,27 +4,27 @@
     - we can solve this problem in tow ways, sorting the list and iterating until we find mismatch or run out of indx and retun the number
     - or convert the list to set and iterate over the range(n+1) and check membership for that number in the set until we find non existing number.
 -  Time and Space complexity:
-    - Time => using sort: O(n log n + (n+1)) = O(n log n),  using set: O(n + n+1) = O(n)
+    - Time = using sort: O(n log n + (n+1)) = O(n log n),  using set: O(n + n+1) = O(n)
     - space = using sort: O(n), using set: O(n)
 """
 
-# Using sort
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        i = 0 
-        while i <= n:
-            if i not in nums:
-                return i
-            i += 1
-
-# # Using set
+# # Using sort
 # class Solution:
 #     def missingNumber(self, nums: List[int]) -> int:
-#         set_nums = set(nums)
-#         for i in range(len(nums)+1):
-#             if i not in set_nums:
+#         nums.sort()
+#         n = len(nums)
+#         i = 0 
+#         while i <= n:
+#             if i not in nums:
 #                 return i
+#             i += 1
+
+# Using set
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        set_nums = set(nums)
+        for i in range(len(nums)+1):
+            if i not in set_nums:
+                return i
             
 
