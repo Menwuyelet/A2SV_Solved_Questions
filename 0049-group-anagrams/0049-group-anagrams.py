1e45866d3c4b1cbee6ba013029b4720f8b329033
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-06 20:00:57 +0300

    Enhance docstring for groupAnagrams function
    Added a detailed docstring explaining the problem, solution approach, and time/space complexity for the groupAnagrams function.

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,3 +1,15 @@
+"""
+- The question: given a list of strings we are tasked to return list of anagram words grouped to gether.(anagram words means they have the same elements but may differ in order)
+- Solution:
+    - so what we can do to solve this problem is check the chrs of each words and group these who has the same chrs.
+    - to do that we can use dictionary and store each word as value with key being its sorted chrs.
+    - this way words that are anagram will be grouped together.
+    - at the end of the loop we just return the dictionary values casted to list.
+-  Time and Space complexity:
+    - Time =  both techniques  O(n*k log k), n = len(strs), k = len(words in strs)
+    - space = using dict: O(n*k)
+"""
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = defaultdict(list)
