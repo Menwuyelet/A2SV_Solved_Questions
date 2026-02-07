917fcd3079f106a4c8f0cb76477aaa50d4bc06ee
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-07 16:09:24 +0300

    Time: 111 ms (64.09%), Space: 74.6 MB (71.27%) - LeetHub

--- 2671-frequency-tracker/2671-frequency-tracker.py
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -1,3 +1,21 @@
+"""
+- The question: we are tasked to implement a FrequencyTracker class that have three methods add, deletone and hasFrequency.
+               - this class will count the frequency of teh data that it stores and perform the three tasks.
+- Solution:
+    - to design this datastructure we use two dictionaries, one to sotre the actual data and their freq and the second to store the amount of elements that are with specific frequency.
+    - so the first dictionary stores the data as key and its frequency as value.
+    - the second will store the frequency as key and number of elements with that specific frequency.
+    - when we add new element we add it to the data dictionary and increment its freq by 1,
+    - after that we subtract 1 from the frequency that specific data had and add 1 to the new frequency the dat now have 
+      (if the prev frequency was 0 meaning the data is new we just add to the new freq which is 1)
+    - when we delete an element we reduce 1 from the current frequency and we add 1 to the new frequency
+      (if the current freq is 1 we just simple reduce 1 from it and do not add to any thing since it is completly of the data and its freq is 0)
+    - when we check if specific freq have a data that appears at that freq we check the second dict and return true if the value is 1 or greater else false.
+-  Time and Space complexity:
+    - Time = O(1) per opration, O(n), n = number of oprations
+    - space = O(k + m) = > O(m), k = number of distinct data, m = frequency range
+"""
+
 class FrequencyTracker:
 
     def __init__(self):
