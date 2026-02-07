833888ecbb53ddda4ea8021f7b019e6138788556
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-07 15:52:27 +0300

    Time: 11 ms (34.52%), Space: 23.4 MB (22.4%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,3 +1,15 @@
+"""
+- The question: given the list of integers we are tasked to return the number that appears more than n//3 times
+- Solution:
+    - to do that we can use dictionary and iterate through each digit and count theire apperance.
+    - after each appearance we check if the number meet the condition
+    - if so we add it to our ans list else we continue.
+    - after the loop ended we return the ans list.
+-  Time and Space complexity:
+    - Time = using sort: O(n), n = len(nums)
+    - space = using sort: O(n), n = len(nume), this is because the ans list will only contain at most 2 elements.
+"""
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         val = len(nums)//3
