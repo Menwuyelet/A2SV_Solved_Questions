30fca58981a2e1d8d076abfbcb5dbca0a3dcc580
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-09 23:34:33 +0300

    Time: 7 ms (54.03%), Space: 23.1 MB (30.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,18 @@
+"""
+- The question: given a list of numbers we are thasked to return the k most repeated numbers.
+- Solution:
+    - to solve this problem we use counter to count the frequency of the numbers and sort them in reverse.
+    - after that we iterate through the sorted list up to k and print the first k elements.
+-  Time and Space complexity:
+    - Time = O(n), n = len(num)
+    - space = O(n)
+"""
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import defaultdict
-        k = 2
-        count = defaultdict(int)
-        for i in nums:
-            count[i] += 1
-        so_count = dict(sorted(count.items(), key=lambda item : item[1], reverse=True))
+        count = Counter(nums)
+        sorted_count = dict(sorted(count.items(), key=lambda item : item[1], reverse=True))
         ans = []
-        for i, key in enumerate(so_count):
+        for i, key in enumerate(sorted_count):
             if i >= k:
                 break
             ans.append(key)
