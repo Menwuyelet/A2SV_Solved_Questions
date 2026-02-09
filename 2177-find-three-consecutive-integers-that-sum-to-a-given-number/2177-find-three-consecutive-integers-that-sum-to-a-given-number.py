2c76e2d3728c0618c6ed7c6655a5feb68454593b
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-09 23:48:37 +0300

    Time: 0 ms (100%), Space: 19.4 MB (12.04%) - LeetHub

--- 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.py
+++ 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.py
@@ -1,3 +1,13 @@
+"""
+- The question: given an integer we are tasked to finde three consecutive integers that are sumed to give that integer.
+- Solution:
+    - to solve this problem, we can use the fact that the number divisible by 3 is also a sum of the quotient, the quotient-1 and the quotient+1 so we return them.
+    - if it is not divisible by 3 then we return [] as there are no such three digits.
+-  Time and Space complexity:
+    - Time = O(1)
+    - space = O(1)
+"""
+
 class Solution:
     def sumOfThree(self, num: int) -> List[int]:
         if num % 3 == 0:
