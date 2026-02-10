bfc1ef451cbbf8c5551c704acf2b020898b98355
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-10 11:43:17 +0300

    Time: 3 ms (78.11%), Space: 19.4 MB (52.02%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -1,3 +1,12 @@
+"""
+- The question: given a list of numbers we are tasked to separate the digits and append them to a lis and return it.
+- Solution:
+    - to do this we just need to iterate over the numbers separate them and append them to our ans list.
+-  Time and Space complexity:
+    - Time = O(n*m), where n = len(nums), m = digits of the number
+    - space = O(m + n),
+"""
+
 class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         ans = []
