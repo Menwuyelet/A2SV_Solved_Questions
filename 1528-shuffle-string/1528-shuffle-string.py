89f4c66ba347a56b09dade24f7c0cf829e33f34c
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-10 14:36:33 +0300

    Time: 0 ms (100%), Space: 19.3 MB (38.66%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,3 +1,12 @@
+"""
+- The question: we are given string and indices and tasked to shuffle the strings to match the given indeces.
+- Solution:
+    - to solve the problem we can create new list initial values and update the values as we iterate through the idices and the string.
+-  Time and Space complexity:
+    - Time = O(n), n = len(s)
+    - space = O(n), 
+"""
+
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         ans = [0]*len(s)
