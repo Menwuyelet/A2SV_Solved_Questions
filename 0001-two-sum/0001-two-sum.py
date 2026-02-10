ce4456f15260d4485c0f780df7660db23a4e1bf4
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-10 13:16:37 +0300

    Time: 3 ms (52.13%), Space: 20.6 MB (12.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,3 +1,16 @@
+"""
+- The question: we are given a list of integers and a target, and we are tasked to find the two numbers that sum up to that target from the given list.
+- Solution:
+    - the brute force solution would be to iterate over all the numbers and find their sum with every other number on the list.
+    - to do that we can use nested loops and iterate over the list n^2 times.
+    - but we have another way, using a dictionary we can store all the numbers in dictionary as key and theire indexe as value.
+    - after that we iterate over the numbers and calculate the diffrence of target - curren number and if that number is present in the dictinary 
+    - that means we found our two numbers and return their indexes.
+-  Time and Space complexity:
+    - Time = O(n), n = len(nums)
+    - space = O(n), 
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numsDic = {}
