1a60e1598b95ded33ff051c4f68a380d2a2e32c3
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-10 11:24:29 +0300

    Time: 7 ms (90.7%), Space: 19.5 MB (27.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,13 @@
+"""
+- The question: given two strings we are tasked to check wether string t is anagram of string s.
+- Solution:
+    - to say two words anagram of eachother they both should have same length and same chr and frequency.
+    - to solve this problem we can use counter to count the frequncies of the chrs in each strings and check wether they are equal or not.
+-  Time and Space complexity:
+    - Time = O(n + n), O(n) n = len(s)
+    - space = O(n),
+"""
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
