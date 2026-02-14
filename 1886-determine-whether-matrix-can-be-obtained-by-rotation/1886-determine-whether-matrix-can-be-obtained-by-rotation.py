3fb5d66ab40e3daf02eb529e0c4793f022029b9f
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-14 20:25:40 +0300

    Time: 1 ms (21.49%), Space: 19.1 MB (72.54%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,4 +1,13 @@
-
+"""
+- The question: we are given two matrixes and we are tasked if matrix mat can be the same as matrix target after rotating it.
+- Solution:
+    - since our constraint is small we can do this by checking for every rotation.
+    - first we try for 90, then 180, and lastly 270
+    - to rotate a matrix we find its transpose and reverse each row.
+-  Time and Space complexity:
+    - Time = O(n^2), n = len(mat)
+    - space = O(1)
+"""
 class Solution(object):
     def findRotation(self, mat, target):
         len_mat = len(mat)
@@ -6,10 +15,12 @@ class Solution(object):
 
         for _ in range(4):
 
+            # transpose the matrix
             for i in range(len_mat):
                 for j in range(i, len_row):
                     mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
-
+           
+            # reverse each rows it to make it rotated
             for row in mat:
                 row.reverse()
 
