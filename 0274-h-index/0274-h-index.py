43206539a54c8aea48334a328f2da2bf4b7689f2
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-21 19:16:07 +0300

    Time: 0 ms (100%), Space: 19.5 MB (52.04%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,14 +1,22 @@
+# class Solution:
+#     def hIndex(self, citations: List[int]) -> int:
+#         citations.sort()
+#         h_indx = 0
+#         n = len(citations)
+
+#         for indx, citation in enumerate(citations):
+#             if (n - indx) >= citation:
+#                 h_indx = citation
+            
+#         return h_indx
+
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort()
-        h_indx = 0
         n = len(citations)
-        
-        if n == 1:
-            return 1
 
-        for indx, citation in enumerate(citations):
-            if (n - indx) + 1 >= citation:
-                h_indx = citation
-            
-        return h_indx
+        for i in range(n):
+            if citations[i] >= n - i:
+                return n - i
+        
+        return 0
