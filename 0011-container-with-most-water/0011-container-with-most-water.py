01b94a0e1d6680db781af790d618b9913dcc645b
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-02-24 18:43:40 +0300

    Time: 56 ms (69.77%), Space: 29.9 MB (12.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,3 +1,14 @@
+"""
+- The question: we are given a list of poles(wals) represented by theire height, we are tasked to find the tanker that is made by two of the wals.
+- Solution:
+    - the brute force solution would be to iterate over all the numbers(wals) and compute the area they generate with each of the other numbers and take the maximum.
+    - but to do that it will take O(n^2), we can solve the problem using O(n) solution.
+    - the solution is simple we use two pointers(coliding) and calculate the area for the two walls and move the pointer with the smalest height.
+    - then we take the maiximum as our answer.
+-  Time and Space complexity:
+    - Time = O(n), n = len(height)
+    - space = O(1), 
+"""
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         left_ptr = 0
@@ -6,12 +17,14 @@ class Solution:
         highest = 0
 
         while left_ptr < right_ptr:
+            # we take the min because we are bound to its height.
             current_height = min(height[left_ptr], height[right_ptr])
             width = right_ptr - left_ptr
 
             current_area = current_height * width
             highest = max(highest, current_area)
             
+            # we move the pointer that holds the minimum height
             if current_height == height[left_ptr]:
                 left_ptr += 1
             else:
