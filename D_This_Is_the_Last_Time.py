ee48ca72440efc335f43c30025979e84e2c53b18
Mnu Temesgen <mnutemesgen@gmail.com>
2026-02-27 19:11:37 +0300

    Refactor solution for "D. This Is the Last Time" problem: streamline logic and improve readability

--- D_This_Is_the_Last_Time.py
+++ D_This_Is_the_Last_Time.py
@@ -1,44 +1,31 @@
-import sys
-import heapq
+"""
+- The question: given a list of casinos we are tasked to maximize the coin we will have at the end of visiting any of the cassinos. possibly zero or all.
 
-input = sys.stdin.readline
+- Solution:
+    - this is really easy question that looks like hard.
+    - what we want to do is take the posible max coin giving casino and visit is and update our coin to that reali.
+    - to do that we can sort it and take the casino with the largest reali.
+    - but the treak is once we update our k, there might be new casino that we can explore and possibly maximize our k.
+    - to check that the brute force would be iterating everytime we update our k to check available cassinos.
+    - but this leads to n^2 time complexity.
+    - we can take this approach and twik it, by sorting the cassinos by thire li and iterating over them and updating our k when we find greater reali
+    - this ensures that even after updating our k we still gona see the newl and already available cassinos ensuring geting the optimal answer.
+-  Time and Space complexity:
+    - Time => O(n log n) due to sorting the list of contests
+    - space = O(n log n), due to sorting
+"""
+for _ in range(int(input())):
+    n, k = map(int,input().split())
+    cassinos = [(list(map(int,input().split()))) for i in range(n)]
 
-t = int(input())
-for _ in range(t):
-    n, k = map(int, input().split())
-    
-    casinos = []
-    for _ in range(n):
-        li, ri, reali = map(int, input().split())
-        casinos.append((li, ri, reali))
-    
-    # Sort by li
-    casinos.sort()
-    
-    current = k
-    i = 0
-    max_heap = []
-    
-    while True:
-        # Add all casinos whose li ≤ current
-        while i < n and casinos[i][0] <= current:
-            li, ri, rieali = casinos[i]
-            heapq.heappush(max_heap, (-reali, ri))
-            i += 1
-        
-        # Remove invaliid ones (where ri < current)
-        while max_heap and max_heap[0][1] < current:
-            heapq.heappop(max_heap)
-        
-        if not max_heap:
-            break
-        
-        best_reali = -max_heap[0][0]
-        
-        if best_reali <= current:
-            break
-        
-        heapq.heappop(max_heap)
-        current = best_reali
-    
-    print(current)
+    # by sorting the increasing order of the li, we insure that we can update our coin (k) and we still be able to process the rest of the list by the updated coin.
+    cassinos.sort()
+
+
+    for li,ri,reali in cassinos:
+
+        # if the current coin is greater than the li of the current cassino and the coin of the cassino is greater than our current k we update our k
+        if k >= li and reali > k:
+            k = reali
+
+    print(k)
