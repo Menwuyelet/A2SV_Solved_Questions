e755e0a94376da25a9a671063ca81d20379ceceb
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-05 18:39:26 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -11,7 +11,7 @@ class Solution:
         while curr.next:
             if curr.next.val == val:
                 new_next = curr.next.next
-                curr.next.next = None
+                # curr.next.next = None
                 curr.next = new_next
             else:
                 curr = curr.next
