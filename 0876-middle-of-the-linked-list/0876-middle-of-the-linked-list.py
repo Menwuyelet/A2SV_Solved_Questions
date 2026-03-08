f84c3705812b04b387a01c18e60d6ba5c3df4c74
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-08 22:55:08 +0300

    Time: 0 ms (100%), Space: 19.3 MB (55.63%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,3 +1,16 @@
+"""
+- The question: we are given a linked list and tasked to return the midle node.
+- Solution:
+    - to achive this we can iterate through all the nodes and construct the index node mapping dictionary.
+    - we can do better using two pointers just like on array
+    - we will have two pointers, one fast and one slow ptr.
+    - we move the fast ptr twice before we we move the slow ptr and by the time the fast ptr reachs the end, the slow reachs the midle
+    - so we can return the node on the slow ptr.
+
+-  Time and Space complexity:
+    - Time = O(n), n = len of the linked list
+    - space = O(1), 
+"""
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -13,12 +26,12 @@ class Solution:
 
         while fast_curr:
             fast += 1
+            
+            # we update our slow_cur only after our fast have updated twice.
             if fast % 2 == 0:
                 slow += 1
                 slow_curr = slow_curr.next
+            
             fast_curr = fast_curr.next
-        
-        # if fast % 2 == 0:
-        #     return slow_curr.next
 
         return slow_curr
