939a6785ff72b4385af41c364e86fe1f3bf5a6fc
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-08 04:39:41 +0300

    Time: 111 ms (70.6%), Space: 33.6 MB (67.27%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -0,0 +1,33 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        # we initiate the matrix with extra column and row for our prfix sum
+        self.prefix_sum = [[0]*(n+1) for _ in range(m+1)]
+
+        # we biuld the prefix sum matrix
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                self.prefix_sum[i][j] = (matrix[i-1][j-1]
+                                            + self.prefix_sum[i-1][j]
+                                            + self.prefix_sum[i][j-1]
+                                            - self.prefix_sum[i-1][j-1])
+        
+        
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+            # i used sub_sum = [r2][r2] - [r2][c1-1] - [r1-1][c2] + [r1-1][c1-2] formula but i tweaked it to correct the extra row and extra column we added during our prefix sum creation by adding 1 to all the indexes
+            return (
+                    self.prefix_sum[row2+1][col2+1]
+                    - self.prefix_sum[row1][col2+1]
+                    - self.prefix_sum[row2+1][col1]
+                    + self.prefix_sum[row1][col1]
+                )
+ 
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
