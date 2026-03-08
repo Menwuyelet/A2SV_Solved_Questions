c378e1d77c4a5143a38c33e72f959f27c220fa07
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-08 20:27:21 +0300

    Time: 0 ms (100%), Space: 22.4 MB (60.52%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -3,20 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+"""
+- The question: we are given a linked list and a value. we are tasked to remove the node with value equal to the given value.
+- Solution:
+    - this is basic linked list traversal problem we just traverse throug the list and remove the node.
+    - to do that we can use dummy node at the begining to be able to remove the frist element as well.
+    - we check the value of the next node when we are on the current node and if it satisfies we point the current nods next to the next nods next. 
+    - this succsesfully removes the node form the linked list.
+
+-  Time and Space complexity:
+    - Time = O(n), n = len of the linked list
+    - space = O(1), 
+"""
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # we use dummy to enable deleting the first
         dummy = ListNode(0)
         dummy.next = head
-        print(head)
+        
+        # traversing variable indicating where we are currently
         curr = dummy
 
-        while curr and curr.next:
+        while curr.next:
             if curr.next.val == val:
                 curr.next = curr.next.next
             # print(curr)
             else:
                 curr = curr.next
         
+        # we return the head 
         return dummy.next
 
 
