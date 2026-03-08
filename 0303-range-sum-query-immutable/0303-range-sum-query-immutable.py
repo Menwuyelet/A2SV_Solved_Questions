ffe3fd1d9c4aecafce02d4b1b0d34060f3104af0
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-08 03:03:04 +0300

    Time: 7 ms (45.35%), Space: 22.7 MB (74.78%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,15 +1,27 @@
+"""
+- The question: we are given a list of numbers and tasked to implement a NumArray class that performs a range sum within o(1) time.
+- Solution:
+    - to do this we could just biuld the prefix sum of the given list and use that to obtain range sum with just one opration.
+-  Time and Space complexity:
+    - Time = O(n), n = len(nums)
+    - space = O(n), 
+"""
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.sum = nums
-        for i in range(1, len(nums)):
-            self.sum[i] = self.sum[i-1] + nums[i]
+        # initiate a prefix sum array with 0 as the first element and extending it with the given array
+        self.nums = [0]
+        self.nums.extend(nums)
 
+        # biulds the prefix sum so we can get the range sum in o(1) by just doing one opration.
+        for i in range(2, len(self.nums)):
+            # we start with 2 because we made offset by adding 0 to the bigining of the prefix sum array
+            self.nums[i] = self.nums[i - 1] + self.nums[i]
+        
     def sumRange(self, left: int, right: int) -> int:
-        if left == 0:
-            return (self.sum[right])
+        # due to the offset we introduced we add 1 to the right ptr to get the right index, and leave the left as it is.
+        return self.nums[right+1] - self.nums[left]
 
-        return(self.sum[right]-self.sum[left-1])
 
 
 # Your NumArray object will be instantiated and called as such:
