b8d84347b086292be5e338bbb401451d9243868d
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-09 17:57:34 +0300

    Time: 19 ms (90.14%), Space: 53.5 MB (30.94%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,3 +1,14 @@
+"""
+- The question: we are given a linked list and tasked to determine wether the given number is palindrom or not
+- Solution:
+    - so to solve this problem we can use two pointers technique and solve it efficiently.
+    - one thing is we cant really use coliding ptrs on a linked list.
+    - so we construct an array from the given linked list and after that we use coliding pointers to check for palindrome.
+-  Time and Space complexity:
+    - Time = O(n), n = length of the given linked list
+    - space = O(n), 
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -8,13 +19,13 @@ class Solution:
         array = []
 
         curr = head
-
+        # iterate through the linked list and construct an array to perform the two ptr approach
         while curr:
             array.append(curr.val)
             curr = curr.next
 
         left, right = 0, len(array)-1
-
+        # we iterate throught the constructed array using left and right ptr and check if it is palindrome or not
         while left < right:
             if array[left] != array[right]:
                 return False
