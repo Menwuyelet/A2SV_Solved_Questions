74e65298cddb4a58688aab86620ccff107b6ba94
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-11 18:30:10 +0300

    Time: 81 ms (96.15%), Space: 20.5 MB (67.83%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -1,3 +1,14 @@
+"""
+- The question: we are given a sting consisting of lower english letters and star. and we are tasked to remove the left most near character when we find a star.
+- Solution:
+    - to solve this we can use stack efficiently.
+    - we can iterate through the string and when we find a non star chr we append it to the stack.
+    - but when we find a star we pop from our stack.
+    - after finishing this we join the stack and return it.
+-  Time and Space complexity:
+    - Time = O(n), n = len(s)
+    - space = O(n), 
+"""
 class Solution:
     def removeStars(self, s: str) -> str:
         stack = []
