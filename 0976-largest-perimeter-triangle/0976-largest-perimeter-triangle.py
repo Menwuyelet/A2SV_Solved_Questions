7e202fccdf0a7f6cb08a0be08e95bfda9f0ebf72
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-12 16:32:22 +0300

    Time: 15 ms (63.32%), Space: 20.4 MB (92.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -3,31 +3,23 @@
 - Solution:
     - to do that we sort the nums to make close numbers in one group as much as possible and to make the formation of valid triangle.
     - after sorting we can iterate through the list and check if the three consiquetive numbers form valid triangle and if they do we compare theire perimeter with the current max and take the maximum.
+    - to avoid all that we could just start at the back of the sorted list and we can return the first valid triplet since that would be the maximum.
+    
 
 -  Time and Space complexity:
     - Time => O(n log n) due to sorting the list
-    - space = O(1), we only use a constant amount of space to store integers
+    - space = O(n log n), we only use a constant amount of space to store integers but the sorting alg takes n log n time
 """
 
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         nums.sort()
-        perimeter = 0
-        left = 0
-        right = 2
-
-        subSum = nums[left] + nums[left+1] + nums[right]
-
-        while right < len(nums):
-            if nums[left] + nums[left+1] > nums[right]:
-                perimeter = max(perimeter, subSum)
-
-            subSum -= nums[left]
-            left +=1
-            right +=1
-            if right < len(nums):
-                subSum += nums[right]
-          
         
-        return perimeter
+        for right in range(len(nums)-1, 1, -1):
+
+            # we first check if the three consequetive numbers form a valid triangle and since we are starting from the back we can return the first triplet
+            if nums[right-2] + nums[right-1] > nums[right]:
+                return nums[right - 2] + nums[right - 1] + nums[right]
+          
+        return 0
                 
