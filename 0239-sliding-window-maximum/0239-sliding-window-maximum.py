046c8a943f165bb9ea3f611d46e1afcbf5f4351d
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-14 20:51:01 +0300

    Time: 191 ms (53.83%), Space: 35.4 MB (43.3%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,3 +1,18 @@
+"""
+- The question: given a list of numbers and a window size k, we are tasked to return a list containing maximum numbers for each of the window when we move the window one step at a time.
+- Solution:
+    - the problem with this type of question is they seems easy enough just to use sliding window and compare the current max with the newly added element.
+    - but the thing is what if we removed the current max out of the window when we moved it.
+    - now we have to find a new local maximum to compare with. and this can happen for all the windows at worst.
+    - so to solve this we could use qeue, specifically monotonically decreasing qeue.
+    - we store indexes in that qeue, for each iteration we check to remove the elements outside of the current window from the qeue.
+    - additionally we check that the newlly added element maintains the monotonicaly decreasing order.
+    - else we start poping from the right until we find an element that is larger than the newlly added element.
+    - after we append our new element to our qeue, we check the window size valid, and if so we append the first element on the que to our ans as it is the current max.
+-  Time and Space complexity:
+    - Time = O(n), n = len(nums)
+    - space = O(n), 
+"""
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         mono_dq = deque()
