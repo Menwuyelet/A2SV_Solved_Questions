98afbaeb74b707fc8d9f513661c80ce4ffae52aa
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-17 18:21:28 +0300

    Time: 0 ms (100%), Space: 19.4 MB (79.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            return 1 / self.base(x, -n)
         
-        return (pow(x, n))
+        return self.base(x, n)
+    
+    def base(self, x, n):
+        if n == 0:
+            return 1
+
+        if n <= 1:
+            return x
+
+        half = self.base(x, n // 2)
+        
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
+
+        
