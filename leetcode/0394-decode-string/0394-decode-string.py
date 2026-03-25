338361ea5b6c07870eecf935ea3cb03c7b30a36f
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-25 18:10:16 +0300

    Add solution for Decode String

--- leetcode/0394-decode-string/0394-decode-string.py
+++ leetcode/0394-decode-string/0394-decode-string.py
@@ -1,21 +1,46 @@
+"""
+- The question: we are given a codeded string with the code format of english letter in angle braces and the number of times they appear in the original string as a cofficient and tasked to return the original string the decoded one.
+
+- Solution:
+    - what we are being tasked is to decode the string. so we can iterate over the string and decode it chr by char.
+    - the problem with this approach is what if there are nested chrs inside of a braces. so we use stack to decode every thing that is enclosed by a braces.
+    - what we do is we iterate through the string and when we hit a number we initiate the decoding state and we append the chr that is inside of the braces multiply it with the coefficient and put it on our ans.
+-  Time and Space complexity:
+    - Time = O(n), n = len(s) since the max num is no more than 300
+    - space = O(n), 
+"""
+
 class Solution:
     def decodeString(self, s: str) -> str:
-        stackCh = []
+        stack = []
+
         for i in range(len(s)):
+            
+            # we append every chr we find from the coded string unless it is closing brace 
             if s[i] != "]":
-                stackCh.append(s[i])
+                stack.append(s[i])
             else:
+                
+                # we pop from our stack until we find the opning brace and construct our sub segment we are decoding
                 sub = ""
-                while stackCh[-1] != "[":
-                    sub = stackCh.pop()+ sub
-                stackCh.pop()
+                while stack[-1] != "[":
+                    sub = stack.pop() + sub
+
+                # we remove the opning brace
+                stack.pop()
+
+                # we construct the multiplying number and convert it in to int
                 num = ""
-                while stackCh and stackCh[-1] in ["0","1","2","3","4","5","6","7","8","9"]:
-                    num = stackCh.pop() + num
+                while stack and stack[-1] in ["0","1","2","3","4","5","6","7","8","9"]:
+                    num = stack.pop() + num
                 
                 inum = int(num)
-                temp = ""
-                for i in range(inum):
-                    temp += sub
-                stackCh.append(temp)
-        return ("".join(stackCh))
+
+                # we decode our segment and add it to our temp ans.
+                temp = sub * inum
+
+                # we put back our temp to our stack so to make sure it is not left out of other possible decodings.
+                stack.append(temp)
+
+        # we retunr our stack after we finished iterating and decoding it
+        return ("".join(stack))
