ee22833f97a4ee12902bfcd752bd64926c304e3f
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-29 16:36:09 +0300

    Add solution for Check if All the Integers in a Range Are Covered

--- leetcode/1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
+++ leetcode/1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -3,7 +3,7 @@ class Solution:
         set_list = set()
 
         for i in ranges:
-            set_list.update(list(range(i[0], i[1]+1)))
+            set_list.update(range(i[0], i[1]+1))
 
         for i in range(left, right+1):
             if i not in set_list:
