a59668cf3ba5708f75dbc3e255c597a3c12d2eae
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-03-31 19:22:54 +0300

    Add solution for First Bad Version

---
+++ leetcode/0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left <= right:
+            mid =   (right + left) // 2
+
+            if isBadVersion(mid) and not isBadVersion(mid-1):
+                return mid
+
+            elif isBadVersion(mid):
+                right = mid - 1
+
+            else:
+                left = mid + 1
