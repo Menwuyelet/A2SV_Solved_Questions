a13ba0fe2f2e906dd6102527b0e7199ef6fdb00e
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-04-11 11:43:37 +0300

    Add solution for The k-th Lexicographical String of All Happy Strings of Length n

--- leetcode/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ leetcode/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,3 +1,15 @@
+"""
+- The question: we are given a list of chrs and the length of the string should be formed by the perituation tasked to return the kth string after forming the permutuations and sorting them alphabeticaly.
+- Solution:
+    - this is an enumrating problem.
+    - we just use backtracking to construct the permituations and after that sort them and return the kth element.
+    - but to avoid sorting, we could just use dictinary to map the elements with an int starting at 1 and return the value mapped to the k.
+    - for further optimizing, we break the backtracking when we hit the kth element removing unassery iterations.
+-  Time and Space complexity:
+    - Time = O(min(2n,k*n)), n = len(nums)
+    - space = O(K * n), 
+"""
+
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         maping = {}
@@ -6,6 +18,9 @@ class Solution:
 
         def backtrack(curr):
             nonlocal i
+            if i > k:
+                return 
+            
             if len(curr) == n:
                 # print(maping)
                 maping[i] = curr[:]
