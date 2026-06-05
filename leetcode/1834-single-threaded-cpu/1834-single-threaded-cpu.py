2ef3aaa31828e56b51de1317ae75761e4c45fa00
Menwuyelet Temesgen <menutemesgen@gmail.com>
2026-06-05 22:36:59 +0300

    Add solution for Single-Threaded CPU

--- leetcode/1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ leetcode/1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,8 +1,25 @@
+"""
+- The question: we are given a list of tasks in a 2d array each task containing its starting time and the duration of time it takes to be processed. we also have a single threaded cpu that process the tasks. and we are tasked to return the list of the tasks based on the processing order.
+- Solution:
+    - this problem seems so simple at first glance, but when we look close we see that it has several constraints hiding.
+    - to start of we need to sort the tasks as they are not given to us sorted,
+    - second, when we go to the next time we don't only process one task, but we take all the tasks that start at that time and we process them based on theire processing time.
+    - third we need to iterate until both we reached last task and there are no available tasks to be processed, else we continue to proccess them.
+    - so to implement this ificiently we need to know which task is the first to be processed in each iteration, this is determind by starting time(availablity) or if there are multiple available tasks by theire time to be processed.
+    - to achive this instant geting of the next task we use heap.
+    - so we iterate over the tasks and we add all currently available tasks to our heap.
+    - after we finish that we pop the top of the heap and add its time to our cpu time to occupy that time and add the task index to our ans, we do taht until we reach end of the task and our heap is empty.
+    - and that is it.
+-  Time and Space complexity:
+    - Time = O(n log n), n = len(tasks)
+    - space = O(n), 
+"""
+
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         n = len(tasks)
 
-        # Add original indices
+        # pack the tasks with theire original index before sorting
         tasks = [(e, p, i) for i, (e, p) in enumerate(tasks)]
         tasks.sort()
 
@@ -12,19 +29,20 @@ class Solution:
         time = 0
         i = 0
 
+        # we iterate until we both reach end of tasks and our heap is empty
         while i < n or heap:
 
-            # If CPU idle, jump to next enqueue time
+            # if CPU is idle, jump to next tasks enqueue time directly
             if not heap and time < tasks[i][0]:
                 time = tasks[i][0]
 
-            # Add all available tasks
+            # we add all currently posible tasks to our heap so we can take the correct one to be processed at this time
             while i < n and tasks[i][0] <= time:
                 e, p, idx = tasks[i]
                 heappush(heap, (p, idx))
                 i += 1
 
-            # Process shortest task
+            # we take the shortest task from our all available tasks in the heap
             p, idx = heappop(heap)
             time += p
             res.append(idx)
